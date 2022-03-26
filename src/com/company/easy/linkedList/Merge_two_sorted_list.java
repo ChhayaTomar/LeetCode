d17c85780d854ebb394e00c2de48d66db13d3faf
ChhayaTomar <tomarchhaya12@gmail.com>
2022-03-27 00:02:51 +0530

    Commit

---
+++ src/com/company/easy/linkedList/Merge_two_sorted_list.java
@@ -0,0 +1,11 @@
+package com.company.easy.linkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Merge_two_sorted_list {
+    public static void main(String[] args) {
+        List<Integer> list1=new LinkedList<>();
+
+    }
+}
