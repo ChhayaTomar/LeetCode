9e3149a12193420d2117bd69bf6a699119c19fe3
ChhayaTomar <tomarchhaya12@gmail.com>
2022-03-27 14:19:16 +0530

    Commit

---
+++ src/com/company/medium/Add_two_numbers.java
@@ -0,0 +1,20 @@
+package com.company.medium;
+
+import java.util.LinkedList;
+
+public class Add_two_numbers {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1=new LinkedList<>();
+        l1.addLast(2);
+        l1.addLast(4);
+        l1.addLast(3);
+        LinkedList<Integer> l2=new LinkedList<>();
+        l2.addLast(5);
+        l2.addLast(6);
+        l2.addLast(4);
+       // addTwoNumbers(l1, l2);
+    }
+   // public static ListNode addTwoNumbers(ListNode l1, ListNode l2){}
+
+}
+
